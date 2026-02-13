bdf0a39980adb37ba8db2fcaeef725eba081f099
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-02-12 23:33:55 -0800

    Component, Jsx,import/export, function call deeplyLearn

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const photo =
+    "https://cdn.imgupscaler.ai/userspace/imgupscaler/image/photo-enhancer/feature_05_enhancer.webp";
+
+  let name = "Ali";
+
+  let user = {
+    name: "Khan",
+    age: 17,
+    email: "khan@gmail.com",
+    city: "karachi",
+  };
+
+  let arrowfun = () => {
+    alert("Arrow Function Triger");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="mt-1">Hello World</h1>
+      <h1 className="mt-1">{name}</h1>
+      <h1 className="mt-1">{user.email}</h1>
+      <button onClick={arrowfun}>Arrow Click</button>
+      <br />
+      <img src={photo} alt={photo} width="100px" className="mt-3" />
     </>
-  )
+  );
 }
 
-export default App
+export function Card() {
+  return (
+    <>
+      <h3>i am card</h3>
+
+      <button
+        onClick={function () {
+          alert("btn clicked");
+        }}
+      >
+        Clicked
+      </button>
+    </>
+  );
+}
+
+export default App;
