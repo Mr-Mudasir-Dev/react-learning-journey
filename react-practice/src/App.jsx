8c1fb453c6b6ecb2942cf8de7a209b9b00493379
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-02-15 22:01:28 -0800

    props learn

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import State from "./state";
+import Student from "./Student";
 function App() {
+  let name = "Ali Akbar";
+  let obj = {
+    name: "Ali",
+    age: 21,
+    city: "Karachi",
+    email: "ali@gmail.com",
+  };
+  let arry = ["Ali", 21, "karachi", +923313213467];
+  function prent(){
+    console.log("Hello Child");
+  }
   return (
     <>
-      <State />
+    
+      <Student variable={name} obj={obj} arr={arry} jsxsentext={<button>Hello child btn</button>} fun={prent()} />
     </>
   );
 }
--- react-practice/src/Student.jsx
+++ react-practice/src/Student.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
-function Student() {
+function Student(props) {
   return (
     <>
+      {console.log(props.variable)}
+      {console.log(props.obj.name)}
+      {console.log(props.obj.age)}
+      {console.log(props.obj.email)}
+      {console.log(props.arr)}
       <hr />
 
+      {props.jsxsentext}
+      {props.fun}
       <h1>Student page</h1>
 
       <hr />
