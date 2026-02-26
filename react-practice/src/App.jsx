adbe1ffd56af5620b3739beddbfb9323622ee24b
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-02-26 02:18:22 -0800

    forword Ref & useFormStatus learn

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,17 +1,60 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Effect from "./effect";
 import Practice from "./Practice";
+import { useRef } from "react";
+import { useFormStatus } from "react-dom";
 
 function App() {
+  const inputforref = useRef();
+
+  const func = (e) => {
+    e.preventDefault();
+    console.log(inputforref);
+    inputforref.current.focus();
+    inputforref.current.style.color = "red";
+  };
+
+  async function formfun() {
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("submited");
+  }
+
+  const Customer = () => {
+    const { pending } = useFormStatus();
+    console.log(pending);
+
+    return (
+      <>
+        <input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <br />
+        <button disabled={pending}>
+          {pending ? "Submitting..." : "Submit"}
+        </button>
+      </>
+    );
+  };
+
   return (
     <>
       <h1>This is App Comonent</h1>
-      <Effect />
+      <br />
+      <h2>this is other component</h2>
+      <form action="">
+        <Practice btnref={inputforref} fun={func} />
+      </form>
       <br />
       <br />
       <br />
-      <Practice />
+      <br />
+
+      <form action={formfun}>
+        <Customer />
+      </form>
     </>
   );
 }
--- react-practice/src/Practice.jsx
+++ react-practice/src/Practice.jsx
@@ -1,51 +1,10 @@
-import { useRef } from "react";
-
-const Practice = () => {
-  const unam = useRef();
-  const upass = useRef();
-
-  function dqsinputref(e) {
-    e.preventDefault();
-    console.log(unam.current.value, upass.current.value);
-  }
-
-  const dqsinput = (event) => {
-    event.preventDefault();
-    console.log(document.querySelector("#unam").value);
-    console.log(document.querySelector("#upass").value);
-  };
-
+const Practice = ({ btnref, fun }) => {
   return (
     <>
-      <hr />
-      <h1>Input Practice Component</h1>
-      <h3>UnControlled Comonent with the help of document.qureeselector</h3>
+      <input ref={btnref} type="text" />
       <br />
       <br />
-      <form action="" onSubmit={dqsinput}>
-        <input type="text" id="unam" />
-        <br />
-        <br />
-        <input type="password" id="upass" />
-        <br />
-        <br />
-        <button>Submit</button>
-      </form>
-      <br />
-      <br />
-      <br />
-      <h3>UnControlled Comonent with the help of Use Ref</h3>
-      <br />
-      <br />
-      <form action="" onSubmit={dqsinputref}>
-        <input type="text" ref={unam} />
-        <br />
-        <br />
-        <input type="password" ref={upass} />
-        <br />
-        <br />
-        <button>Submit</button>
-      </form>
+      <button onClick={fun}>Submit</button>
     </>
   );
 };
