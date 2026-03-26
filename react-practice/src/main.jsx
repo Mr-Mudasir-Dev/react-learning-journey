b3414920e615b5c3742fff53f33088c1b3d58909
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-03-25 22:04:04 -0700

    validation useForm/zod

--- react-practice/src/main.jsx
+++ react-practice/src/main.jsx
@@ -1,10 +1,4 @@
 import { createRoot } from "react-dom/client";
-// import { StrictMode } from "react";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+createRoot(document.getElementById("root")).render(<App />);
